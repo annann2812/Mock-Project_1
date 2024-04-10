ee1979abfa274af876a59ced5dc145f9b50116c9
Hieutn29 <hieutran290611@gmail.com>
2024-04-10 18:49:48 +0700

    no message

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,35 +1,37 @@
-import { Box } from '@mui/material';
-import { ReactNode } from 'react'
-import SidebarList from '../components/Sidebar/SidebarList';
-import SidebarProduct from '../components/Sidebar/SidebarProduct';
+import { Box } from "@mui/material";
+import { ReactNode } from "react";
+import SidebarList from "../components/Sidebar/SidebarList";
+import SidebarProduct from "../components/Sidebar/SidebarProduct";
 
 interface LayoutProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
-const Layout = ({children} : LayoutProps) => {
+const Layout = ({ children }: LayoutProps) => {
   return (
-    <Box sx={{
+    <Box
+      sx={{
         backgroundColor: "#fff",
         display: "flex",
         flexDirection: {
-            // xs: "column",
-            lg: "row"
+          // xs: "column",
+          lg: "row",
         },
         color: "#000",
         padding: 1,
         overflowY: "hidden",
         gap: 3,
-    }}>
-        <Box>
-          <SidebarList/>
-          <SidebarProduct/>
-
-        </Box>
-        <Box sx={{ width:"100%"}}>{children}</Box>
-        {/* coment */}
+      }}
+    >
+      <Box>
+        <SidebarList />
+        <SidebarProduct />
+      </Box>
+      <Box sx={{ width: "100%" }}>{children}</Box>
+      {/* coment */}
+      {/* comt2 */}
     </Box>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
