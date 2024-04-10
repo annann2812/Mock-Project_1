eba412a4c53cd7a2b85221561156c764ccf57a4b
danghung2216 <160234947+danghung2216@users.noreply.github.com>
2024-04-10 18:56:27 +0700

    no message

--- src/pages/Store/Store.tsx
+++ src/pages/Store/Store.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
 
 const Store = () => {
   return (
     <div>Store</div>
-  )
-}
+    // jasjdhkjas
+  );
+};
 
-export default Store
+export default Store;
