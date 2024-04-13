ff973a6ba6ad5c96932a48c080ad9619a6b5e8d3
DESKTOP-DT126GM\dangh <danghung2216@gmai.com>
2024-04-13 07:11:52 +0700

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "../public/routes";
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return;
+  <RouterProvider router={router} />;
 };
 
 export default App;
