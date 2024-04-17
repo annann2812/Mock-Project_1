3af5d92b3cacb822e87a353637e8f87c2dadf276
NguyenTuan3011 <tuanatwork3011@gmail.com>
2024-04-17 19:33:41 +0700

    Add Filter, fix bug, remove some spare component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import { Route, RouterProvider, Routes } from "react-router-dom";
-
-import { router } from "./routes";
+import { Route, Routes } from "react-router-dom";
 import { Fragment } from "react/jsx-runtime";
 import Header from "./components/Header_Footer/Header";
 import Footer from "./components/Header_Footer/Footer";
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./pages/Home/Home";
-import BestSellerProduct from "./components/BestSeller_NewArrival-Product/BestSellerProduct";
+
 import Cart from "./pages/Cart/Cart";
 import Contact from "./pages/Contact/Contact";
 import BestSellers from "./pages/BestSellers/BestSellers";
 import NewArrival from "./pages/NewArrival/NewArrival";
 import BlogPage from "./pages/Blog/Blog";
 
-
 const App = () => {
   return (
     <Fragment>
       <Header />
       <NavBar />
-      {/* <RouterProvider router={router}/> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/best-sellers" element={<BestSellers />} />
--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,11 +1,7 @@
 import { Fragment, ReactNode } from "react";
 import SidebarList from "../components/Sidebar/SidebarList";
 import SidebarProduct from "../components/Sidebar/SidebarProduct";
-import Header from "../components/Header_Footer/Header";
 import Footer from "../components/Header_Footer/Footer";
-import NewArrivalProduct from "../components/BestSeller_NewArrival-Product/NewArrivalProduct";
-import NavBar from "../components/NavBar/NavBar";
-
 
 interface LayoutProps {
   children: ReactNode;
@@ -14,7 +10,6 @@ interface LayoutProps {
 const Layout = ({ children }: LayoutProps) => {
   return (
     <Fragment>
-      <Header />
       <div className="bg-white flex text-black overflow-hidden gap-8 p-3 w-full xl:w-[1280px] mx-auto">
         <div className="w-2/6">
           <SidebarList />
