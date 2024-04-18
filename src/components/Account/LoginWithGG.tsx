fa808baf33593249a4d9b4a4d1717426de0a9de6
Hieutn29 <hieutran290611@gmail.com>
2024-04-19 01:06:35 +0700

    no message

--- src/components/Account/LoginWithGG.tsx
+++ src/components/Account/LoginWithGG.tsx
@@ -8,18 +8,20 @@ const LoginWithGG = () => {
   const navigate = useNavigate();
   return (
     <Fragment>
-      <GoogleOAuthProvider clientId="846149742047-ejle7gbl04nb33s2kfb97p6om7oibfto.apps.googleusercontent.com">
-        <GoogleLogin
-          onSuccess={(credentialResponse: CredentialResponse) => {
-            const decoded = jwtDecode(credentialResponse.credential);
-            window.sessionStorage.setItem("usernameGG", decoded.name);
-            navigate("/");
-          }}
-          onError={() => {
-            console.log("Login Failed");
-          }}
-        />
-      </GoogleOAuthProvider>
+      <div className="w-25 mx-auto mt-5 pt-5">
+        <GoogleOAuthProvider clientId="846149742047-ejle7gbl04nb33s2kfb97p6om7oibfto.apps.googleusercontent.com">
+          <GoogleLogin
+            onSuccess={(credentialResponse: CredentialResponse) => {
+              const decoded = jwtDecode(credentialResponse.credential);
+              window.sessionStorage.setItem("usernameGG", decoded.name);
+              navigate("/");
+            }}
+            onError={() => {
+              console.log("Login Failed");
+            }}
+          />
+        </GoogleOAuthProvider>
+      </div>
     </Fragment>
   );
 };
