6dc9aee0a958048f8d500ca68312a0274eee365f
An Nguyen <128132897+annann2812@users.noreply.github.com>
2024-04-26 06:17:09 +0700

    Update HeroSlider.tsx

--- src/components/Carousel/HeroSlider.tsx
+++ src/components/Carousel/HeroSlider.tsx
@@ -4,31 +4,30 @@ import Hero1 from "../../assets/image/HeroSlider_1.jpeg"
 import Hero2 from "../../assets/image/HeroSilder_2.jpeg"
 import Hero3 from "../../assets/image/HeroSlider_3.jpeg"
 
-
 const slides = [
   {
-    imgSrc: "https://m.media-amazon.com/images/I/61oSyFbfluL._AC_SX395_.jpg",
-    altText: "Golf Shoes",
-    title: "Golf Shoes",
-    description: "Description for Golf Shoes",
+    imgSrc: Hero1,
+    altText: 'Image 1',
+    title: 'Slide 1',
+    description: 'Description for Slide 1',
   },
   {
-    imgSrc: "https://m.media-amazon.com/images/I/51mNlxDSv1L._AC_SY395_.jpg",
-    altText: "Image 2",
-    title: "Ballet Flats",
-    description: "Description for Ballet Flats",
+    imgSrc: Hero2,
+    altText: 'Image 2',
+    title: 'Slide 2',
+    description: 'Description for Slide 2',
   },
   {
-    imgSrc: "https://m.media-amazon.com/images/I/51yfg7sxn4L._AC_SX395_.jpg",
-    altText: "Image 3",
-    title: "Espadrille Wedges",
-    description: "Description for Espadrille Wedges",
+    imgSrc: Hero3,
+    altText: 'Image 3',
+    title: 'Slide 3',
+    description: 'Description for Slide 3',
   },
 ];
 
 const Hero: React.FC = () => {
   return (
-    <div className="w-70 h-50">
+    <div className="w-70">
       <SliderHero slides={slides} />
     </div>
   );
